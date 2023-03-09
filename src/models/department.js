0600d37fe8d76d0b5fd1b88128150d44797411e4
JulianSandoval25 <juliansandoval1996@gmail.com>
2023-03-09 10:37:37 -0300

    Update schema departamento

--- src/models/department.js
+++ src/models/department.js
@@ -6,6 +6,11 @@ const departmentSchema = new mongoose.Schema({
   fechaPublicacion: { type: Date, required: true, default: Date.now },
   disponible: { type: Boolean, required: true, default: true },
   tipo: { type: String, required: true, enum: ['venta', 'alquiler'] },
+  calle: { type: String, required: true },
+  ciudad: { type: String, required: true },
+  provincia: { type: String, required: true },
+  codigoPostal: { type: String, required: true },
+  pais: { type: String, required: true, default: "Argentina" },
   comentarios: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment', required: false }]
 },{
     timestamps:true,
