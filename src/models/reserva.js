6874a7a8de4880a07d4db6ee51c426a032016644
JulianSandoval25 <juliansandoval1996@gmail.com>
2023-03-17 00:52:07 -0300

    add

--- src/models/reserva.js
+++ src/models/reserva.js
@@ -1,8 +1,8 @@
 import { mongoose } from 'mongoose';
 
 const reservaSchema = new mongoose.Schema({
-  usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  propiedad: { type: mongoose.Schema.Types.ObjectId, ref: 'Department', required: true },
+  usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, onDelete: 'CASCADE' },
+  propiedad: { type: mongoose.Schema.Types.ObjectId, ref: 'Department', required: true, onDelete: 'CASCADE' },
   fecha: { type: Date, required: true },
   hora: { type: String, required: true },
   duracion: { type: Number, required: false },// vers si se tiene que eliminar
